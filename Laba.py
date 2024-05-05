04f85fba05a7abc03d8f392b04a3304ae560316c
user <andrey.gromov.36912@gmail.com>
2024-05-05 20:09:52 +0300

    Урок

--- Laba.py
+++ Laba.py
@@ -1,70 +1,48 @@
-# Завдання 2
-# Реалізація програми для додавання, видалення та
-# відстеження завдань/заміток. Зберігати ці завдання у
-# форматі JSON у файлі. Можливість завантаження
-# раніше збережених завдань для подальшої роботи з
-# ними.
+# Завдання 3
+# До вже реалізованого класу «Дріб» додайте можливість стиснення та розпакування даних з
+# використанням json та pickle.
 
 import json
+import pickle
 
+class Дріб:
+    def __init__(self, чисельник, знаменник):
+        self.чисельник = чисельник
+        self.знаменник = знаменник
 
-def load_tasks(filename):
-    try:
+    def стиснути_JSON(self, filename):
+        with open(filename, 'w') as f:
+            json.dump({"чисельник": self.чисельник, "знаменник": self.знаменник}, f)
+
+    @classmethod
+    def розпакувати_JSON(cls, filename):
         with open(filename, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
+            data = json.load(f)
+        return cls(data["чисельник"], data["знаменник"])
 
+    def стиснути_pickle(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump((self.чисельник, self.знаменник), f)
 
-def save_tasks(tasks, filename):
-    with open(filename, 'w') as f:
-        json.dump(tasks, f)
+    @classmethod
+    def розпакувати_pickle(cls, filename):
+        with open(filename, 'rb') as f:
+            чисельник, знаменник = pickle.load(f)
+        return cls(чисельник, знаменник)
 
-
-def add_task(task, tasks):
-    tasks.append(task)
-    print("Завдання додано.")
-
-
-def remove_task(task_index, tasks):
-    if 0 <= task_index < len(tasks):
-        removed_task = tasks.pop(task_index)
-        print("Завдання '{}' видалено.".format(removed_task))
-    else:
-        print("Недійсний індекс завдання.")
-
-
-def display_tasks(tasks):
-    print("Ваші завдання:")
-    for i, task in enumerate(tasks):
-        print("{}. {}".format(i + 1, task))
-
+    def __str__(self):
+        return f"{self.чисельник}/{self.знаменник}"
 
 if __name__ == "__main__":
-    filename = "tasks.json"
-    tasks = load_tasks(filename)
 
-    while True:
-        print("\nМеню:")
-        print("1. Додати завдання")
-        print("2. Видалити завдання")
-        print("3. Відобразити всі завдання")
-        print("4. Вийти")
+    дріб1 = Дріб(3, 4)
+    дріб1.стиснути_JSON("дріб1.json")
 
-        choice = input("Виберіть дію: ")
+    дріб2 = Дріб.розпакувати_JSON("дріб1.json")
+    print("Дріб 2 (збережений та завантажений з JSON):", дріб2)
 
-        if choice == "1":
-            task = input("Введіть нове завдання: ")
-            add_task(task, tasks)
-        elif choice == "2":
-            task_index = int(input("Введіть індекс завдання, яке потрібно видалити: ")) - 1
-            remove_task(task_index, tasks)
-        elif choice == "3":
-            display_tasks(tasks)
-        elif choice == "4":
-            save_tasks(tasks, filename)
-            print("Дані збережено. До побачення!")
-            break
-        else:
-            print("Недійсний вибір. Будь ласка, виберіть знову.")
+    дріб3 = Дріб(5, 6)
+    дріб3.стиснути_pickle("дріб3.pkl")
 
+    дріб4 = Дріб.розпакувати_pickle("дріб3.pkl")
+    print("Дріб 4 (збережений та завантажений з Pickle):", дріб4)
